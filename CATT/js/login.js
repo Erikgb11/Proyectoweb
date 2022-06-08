0e05133543d04910266c673beb47161fe59aa65f
JorgeOchoaVel <89050423+JorgeOchoaVel@users.noreply.github.com>
2022-06-08 00:10:34 -0500

    Add files via upload

---
+++ CATT/js/login.js
@@ -0,0 +1,35 @@
+$(document).ready(()=>{
+    $("form#formLogin").validetta({
+    bubblePosition: 'bottom',
+    onValid:(e)=>{
+        e.preventDefault();
+        $.ajax({
+            method:"post",
+            url:"./pages/login_AX.php",
+            data:$("form#formLogin").serialize(),
+            cache:false,
+            success:(respAX)=>{
+                let AX = JSON.parse(respAX);
+                if(AX.cod ==1){
+                    Swal.fire({
+                        title: 'CATT',
+                        html: AX.msj,
+                        icon: 'success',
+                        confirmButtonText: 'OK',
+                        didDestroy:()=>{
+                            window.location.href = "./pages/inicio_alumno.php";
+                        }
+                    });
+                }else{
+                    Swal.fire({
+                        title: 'CATT',
+                        html: AX.msj,
+                        icon: 'error',
+                        confirmButtonText: 'OK',
+                    });
+                }
+            }
+        });
+    }
+});
+});
