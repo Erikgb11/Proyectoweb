2afefb3457d67a16c70adc0efd90f4272bd8916b
JorgeOchoaVel <89050423+JorgeOchoaVel@users.noreply.github.com>
2022-06-09 03:49:10 -0500

    Update login_AX.php

--- CATT/pages/login_AX.php
+++ CATT/pages/login_AX.php
@@ -4,14 +4,29 @@
     $boleta = $_POST["boleta"];
     $pass = md5($_POST["pass"]);
 
-    $sqlCheckAlmn = "SELECT * FROM alumno WHERE boleta = '$boleta' AND contrasena = '$pass'";
-    $resCheckAlmn = mysqli_query($conexion,$sqlCheckAlmn);
-    if(mysqli_num_rows($resCheckAlmn) == 1){
-        $infAlmn = mysqli_fetch_row($resCheckAlmn);
+    if(substr($boleta, 0, 1) == "2" ){
+        $sqlCheckUsuario = "SELECT * FROM alumno WHERE boleta = '$boleta' AND contrasena = '$pass'";
+        $resCheckUsuario = mysqli_query($conexion,$sqlCheckUsuario);
+        $infUsuario = mysqli_fetch_row($resCheckUsuario);
         $respAX["cod"] = 1;
-        $respAX["msj"] = "<h5>Bienvenido $infAlmn[0]</h5>";
+    }
+
+    if(substr($boleta, 0, 1) == "S" ){
+        $sqlCheckUsuario = "SELECT * FROM profesor WHERE id_prof = '$boleta' AND contrasena = '$pass'";
+        $resCheckUsuario = mysqli_query($conexion,$sqlCheckUsuario);
+        $infUsuario = mysqli_fetch_row($resCheckUsuario);
+        if($infUsuario[8]=="1"){//administrador
+            $respAX["cod"] = 2;
+        }else{
+            $respAX["cod"] = 3;
+        }
+    }
+
+    if(mysqli_num_rows($resCheckUsuario) == 1){
+        $respAX["msj"] = "<h5>Bienvenido $infUsuario[1]</h5>";
         $_SESSION["boleta"] = $boleta;
-    }else{
+    }
+    else{
         $respAX["cod"] = 0;
         $respAX["msj"] = "<h5>Error. Verificar los datos proporcionados</h5>";
     }
