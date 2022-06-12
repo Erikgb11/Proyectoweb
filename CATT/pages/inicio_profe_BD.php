2376dbdc715c1ae4d199b83ddc5f6a0728c2d407
JorgeOchoaVel <89050423+JorgeOchoaVel@users.noreply.github.com>
2022-06-12 02:40:35 -0500

    Delete CATT/pages directory

--- CATT/pages/inicio_profe_BD.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("./configBD.php");
-$boleta = $_SESSION["boleta"];
-$sqlGetNombre = "SELECT * FROM profesor WHERE id_prof = '$boleta'";
-$resGetNombre = mysqli_query($conexion, $sqlGetNombre);
-$infGetNombre = mysqli_fetch_row($resGetNombre);
-?>
