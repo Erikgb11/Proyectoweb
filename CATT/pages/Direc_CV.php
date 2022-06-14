2e4f56b9c3ba26f30fd6ff004fc18c76c0b48829
JorgeOchoaVel <89050423+JorgeOchoaVel@users.noreply.github.com>
2022-06-13 22:36:00 -0500

    Delete CATT/pages directory

--- CATT/pages/Direc_CV.php
+++
@@ -1,4 +0,0 @@
-<?php
-$archivo = $_FILES["archivo"];
-$resultado = move_uploaded_file($archivo["tmp_name"],'../formatos/Direc_CV/'.  $archivo["name"]);
-?>
--- CATT/pages/cerrarSesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    $nombreSesion = $_GET["sesion"];
-    unset($_SESSION["$nombreSesion"]);
-    header("location:./../index.html");
-?>
