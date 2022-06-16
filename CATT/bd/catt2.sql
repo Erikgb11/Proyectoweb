6a6ca9493427ef50f2edd56068e2087883a4b414
Angel Tellez <61524070+atr-ipn@users.noreply.github.com>
2022-06-16 01:04:34 -0500

    Add files via upload

--- CATT/bd/catt2.sql
+++ CATT/bd/catt2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-06-2022 a las 04:59:49
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 16-06-2022 a las 08:04:09
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `zprba`
+-- Base de datos: `catt`
 --
 
 -- --------------------------------------------------------
@@ -44,15 +44,14 @@ CREATE TABLE `alumno` (
 --
 
 INSERT INTO `alumno` (`boleta`, `nombre`, `apellidos`, `correo`, `telefono`, `contrasena`, `auditoria`, `id_rol`, `carrera`) VALUES
-('2022630001', 'Juan Manuel', 'Díaz Díaz', 'juan@hotmail.com', '5500000001', '202cb962ac59075b964b07152d234b70', '2022-06-15 00:06:17', '3', 'ISC'),
-('2022630002', 'David ', 'Martínez Martínez', 'david@gmail.com', '5500000002', '202cb962ac59075b964b07152d234b70', '2022-06-15 00:07:42', '3', 'ISC'),
-('2022630003', 'Ana', 'Domínguez  Domínguez', 'Ana@gmail.com', '5500000003', '202cb962ac59075b964b07152d234b70', '2022-06-15 00:08:27', '3', 'ISC'),
-('2022630004', 'Luis Alberto', 'Arellano Arellano', 'alberto_luis@hotmail.com', '5500000004', '202cb962ac59075b964b07152d234b70', '2022-06-15 00:15:39', '3', 'LCD'),
-('2022630005', 'Ximena ', 'Cruz Cruz', 'ximena.c@gmail.com', '5500000005', '202cb962ac59075b964b07152d234b70', '2022-06-15 00:17:02', '3', 'IIA'),
+('2022350279', 'Karen', 'López', 'Karen@hotmail.com', '1234567890', '81dc9bdb52d04dc20036dbd8313ed055', '2022-06-15 03:53:58', '1', 'ISC'),
+('2022630001', 'Juan Manuel', 'Díaz Díaz', 'juan@hotmail.com', '5500000001', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-06-15 00:06:17', '3', 'ISC'),
+('2022630002', 'David ', 'Martínez Martínez', 'david@gmail.com', '5500000002', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-06-15 00:07:42', '3', 'ISC'),
+('2022630003', 'Ana', 'Domínguez  Domínguez', 'Ana@gmail.com', '5500000003', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-06-15 00:08:27', '3', 'ISC'),
+('2022630004', 'Luis Alberto', 'Arellano Arellano', 'alberto_luis@hotmail.com', '5500000004', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-06-15 00:15:39', '3', 'LCD'),
+('2022630005', 'Ximena ', 'Cruz Cruz', 'ximena.c@gmail.com', '5500000005', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-06-15 00:17:02', '3', 'IIA'),
 ('2022630007', 'Eduardo', 'Rojas Rojas', 'Eduardo@gmail.com', '5500000006', NULL, '2022-06-15 00:25:59', '3', 'ISC'),
-('2022630008', 'Angelica Alejandra', 'Anaya Anaya', 'angelica@gmail.com', '5500000007', NULL, '2022-06-15 00:25:59', '3', 'ISC'),
-('2022630009', 'Kevin', 'Arellano Díaz', 'Diana@diaz.com', '5591023984', '202cb962ac59075b964b07152d234b70', '2022-06-15 21:08:20', '3', 'IIA'),
-('2022630013', 'Juan', 'Díaz Díaz', 'Juan@juan.com', '5591023984', '202cb962ac59075b964b07152d234b70', '2022-06-15 21:34:16', '3', 'IIA');
+('2022630008', 'Angelica Alejandra', 'Anaya Anaya', 'angelica@gmail.com', '5500000007', NULL, '2022-06-15 00:25:59', '3', 'ISC');
 
 -- --------------------------------------------------------
 
@@ -71,7 +70,9 @@ CREATE TABLE `director` (
 --
 
 INSERT INTO `director` (`registro`, `id_TT`, `id_prof`) VALUES
-(1, '2022630001', '1993630001');
+(1, '2022630001', '1993630001'),
+(2, '2022630002', '1993630005'),
+(47, '2022630003', '1993630003');
 
 -- --------------------------------------------------------
 
@@ -92,7 +93,9 @@ CREATE TABLE `equipo` (
 INSERT INTO `equipo` (`registro`, `id_TT`, `boleta`) VALUES
 (1, '2022630001', '2022630001'),
 (2, '2022630001', '2022630007'),
-(3, '2022630001', '2022630008');
+(3, '2022630001', '2022630008'),
+(4, '2022630002', '2022350279'),
+(58, '2022630003', '2022630004');
 
 -- --------------------------------------------------------
 
@@ -101,7 +104,7 @@ INSERT INTO `equipo` (`registro`, `id_TT`, `boleta`) VALUES
 --
 
 CREATE TABLE `profesor` (
-  `id_prof` varchar(11) NOT NULL,
+  `id_prof` varchar(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) DEFAULT NULL,
   `correo` varchar(250) NOT NULL,
@@ -117,15 +120,14 @@ CREATE TABLE `profesor` (
 --
 
 INSERT INTO `profesor` (`id_prof`, `nombre`, `apellido`, `correo`, `telefono`, `contrasena`, `cv`, `auditoria`, `id_rol`) VALUES
-('1993630000', 'CATT', NULL, 'CATT@hotmail.com', '5512345678', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-08 22:15:40', '1'),
-('1993630001', 'Roberto ', 'Pérez Pérez', 'rober@gmail.com', '555500002', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:10:28', '2'),
-('1993630001T', 'José Luis', 'López López', 'jose.luis@gmail.com', '555500001', '202cb962ac59075b964b07152d234b70', '1', '2022-06-15 00:09:30', '2'),
-('1993630002', 'Carlos', 'Silva Silva', 'carlos@hotmail.com', '555500003', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:11:10', '2'),
-('1993630002T', 'José', 'Luis', 'jose.luis@gmail.com', '5591023984', '202cb962ac59075b964b07152d234b70', '1', '2022-06-15 21:36:07', '2'),
-('1993630003', 'Karla Patricia', 'Ramírez Ramírez ', 'karla.p@gmail.com', '555500004', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:12:27', '2'),
-('1993630004', 'Ana Victoria', 'García García', 'victoria@hotmail.com', '555500005', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:18:11', '2'),
-('1993630005', 'Luz María', 'Silva Pérez', 'maria@gmail.com', '555500006', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:20:11', '2'),
-('1993630006', 'Leonardo', 'Martínez López', 'leo.martinez@gmail.com', '555500007', '202cb962ac59075b964b07152d234b70', NULL, '2022-06-15 00:21:13', '2');
+('1993630000', 'CATT', NULL, 'CATT@hotmail.com', '5512345678', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-08 22:15:40', '1'),
+('1993630001', 'Roberto ', 'Pérez Pérez', 'rober@gmail.com', '555500002', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:10:28', '2'),
+('1993630002', 'Carlos', 'Silva Silva', 'carlos@hotmail.com', '555500003', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:11:10', '2'),
+('1993630003', 'Karla Patricia', 'Ramírez Ramírez ', 'karla.p@gmail.com', '555500004', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:12:27', '2'),
+('1993630004', 'Ana Victoria', 'García García', 'victoria@hotmail.com', '555500005', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:18:11', '2'),
+('1993630005', 'Luz María', 'Silva Pérez', 'maria@gmail.com', '555500006', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:20:11', '2'),
+('1993630006', 'Leonardo', 'Martínez López', 'leo.martinez@gmail.com', '555500007', 'd8578edf8458ce06fbc5bb76a58c5ca4', NULL, '2022-06-15 00:21:13', '2'),
+('1993631001', 'José Luis', 'López López', 'jose.luis@gmail.com', '555500001', 'd8578edf8458ce06fbc5bb76a58c5ca4', '1', '2022-06-15 00:09:30', '2');
 
 -- --------------------------------------------------------
 
@@ -154,10 +156,21 @@ INSERT INTO `rol` (`id_rol`, `descripcion`) VALUES
 --
 
 CREATE TABLE `sinodal` (
-  `id_sinodal` varchar(11) NOT NULL,
+  `id_sinodal` int(10) NOT NULL,
   `id_TT` varchar(11) NOT NULL,
   `id_prof` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sinodal`
+--
+
+INSERT INTO `sinodal` (`id_sinodal`, `id_TT`, `id_prof`) VALUES
+(2, '2022630001', '1993630005'),
+(3, '2022630002', '1993630006'),
+(8, '2022630001', '1993630002'),
+(10, '2022630003', '1993630004'),
+(11, '2022630002', '1993631001');
 
 -- --------------------------------------------------------
 
@@ -177,8 +190,9 @@ CREATE TABLE `tt` (
 --
 
 INSERT INTO `tt` (`id_TT`, `titulo`, `resumen`, `pdf`) VALUES
-('2022630001', 'Agente Inteligente para capacitación de personal de TI. 11111', 'HOLAAAA Desarrollar un agente inteligente tipo chatbot para proveer capacitación básica al personal que recién ingresa a un área operativa de TI.', ''),
-('2022630002', 'Hola', 'Hola', '');
+('2022630001', 'Agente Inteligente para capacitación de personal de TI.', 'Desarrollar un agente inteligente tipo chatbot para proveer capacitación básica al personal que recién ingresa a un área operativa de TI.', ''),
+('2022630002', 'Orientador virtual para la elección de productos.', 'Desarrollar una aplicación que genere recomendaciones de productos con base en los perfiles, preferencias, calificaciones, opiniones de usuarios y tipos de clientes.', ''),
+('2022630003', 'Sistema de Incidencia para ESCOM.', 'Desarrollar un sistema que permita la administración de incidencias del personal académico de ESCOM, utilizando las tecnologías disponibles en esta unidad académica.', '');
 
 --
 -- Índices para tablas volcadas
@@ -248,7 +262,13 @@ ALTER TABLE `director`
 -- AUTO_INCREMENT de la tabla `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT de la tabla `sinodal`
+--
+ALTER TABLE `sinodal`
+  MODIFY `id_sinodal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Restricciones para tablas volcadas
